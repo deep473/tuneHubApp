79eea8dbe4518c81fb6bb41f17e5108b14ae173c
Deep <148319264+deep473@users.noreply.github.com>
2024-02-14 11:39:04 +0530

    Project files as on 14th Feb, 2024.

---
+++ NavController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavController 
+{
+	@GetMapping("/map-register")
+	public String registerMapping()	
+	{
+		return "register";
+	}
+	
+	@GetMapping("/map-login")
+	public String loginMapping()	
+	{
+		return "login";
+	}
+	
+	@GetMapping("/map-songs")
+	public String songMapping()	
+	{
+		return "addsongs";
+	}
+	
+	@GetMapping("/samplePayment")
+	public String samplePayment()	
+	{
+		return "samplePayment";
+	}
+	
+	
+	
+}
---
+++ SongsController.java
@@ -0,0 +1,74 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entities.Songs;
+import com.example.demo.services.SongsService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class SongsController {
+	@Autowired
+	SongsService songserv;
+
+	@PostMapping("/addsongs")
+	public String addSongs(@ModelAttribute Songs song){
+		boolean status = songserv.songExists(song.getName());
+		if(status==false)
+		{
+			songserv.addSongs(song);
+			return "songsuccess";
+		}
+		else
+		{
+			return "songfail";
+		}		
+	}
+
+	@GetMapping("/map-viewsongs")
+	public String viewSongs(Model model) {
+		List<Songs> songslist = songserv.fetchAllSongs();
+		model.addAttribute("songslist", songslist);
+		return "displaysongs";
+	}
+
+	@GetMapping("/viewsongs")
+	public String viewCustomerSongs(Model model) {
+		boolean primeCustomerStatus=true;
+		if(primeCustomerStatus==true) {
+			List<Songs> songslist = songserv.fetchAllSongs();
+			model.addAttribute("songslist", songslist);
+			return "displaysongs";
+		}
+		else {
+			return "makepayment";
+		}
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
